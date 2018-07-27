d561cfe17573c3c733c930916634eaa797835c12
kwaleko <khaledalomar@tutanota.com>
2018-07-28 00:37:14 +0300

    refactor

--- src/Core/Leaves.hs
+++ src/Core/Leaves.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE LambdaCase #-}
 module Core.Leaves where
 
 import Core.Types
@@ -8,99 +10,49 @@ import Data.Time(addGregorianMonthsClip
                 ,toGregorian
                 ,fromGregorian)
 import Data.Tuple.Extra
-import Time.Types
-import Data.Hourglass
+--import Time.Types
+--import Data.Hourglass
 
 
 class Monad m => HasLeave m where
   createLeave :: LeaveRequest -> m (Maybe LeaveId)
 
-mm = dateMonth
-yyyy = dateYear
-dd = dateDay
 
-adjCreation :: LeaveRequest -> [AbsenceJournal]
-adjCreation = undefined
+absCreation :: LeaveRequest -> Maybe AbsenceJournal
+absCreation leave = case reqWorkflowState leave of
+  Approved ->
+    let
+      empNb   = reqEmpl leave
+      fromDate = reqFromDate leave
+      toDate   = reqToDate   leave
+      days     = split fromDate toDate
+    in
+      Just $ AbsenceJournal  "" []  ""
+  _        -> Nothing
 
+-- the date for a leave request should be splited on
+-- a monthly basis so the payrun could deduct the
+-- appropriate amount based on the number of days
 split :: Day -> Day -> [(Day,Day)]
-split fdate tdate = if lstMDay fdate > tdate
+split fdate tdate = if lstMDay fdate >= tdate
   then (:) (fdate,tdate)  []
   else (:) tuple rem
   where
     tuple = (,) fdate $ lstMDay fdate
     rem   = split (addGregorianMonthsClip 1 $ fstMDay fdate) tdate
 
+setDay :: Int -> (Int -> Int) -> Day -> Day
+setDay nb fn day = let
+  (year,month,_) = toGregorian day
+  in fromGregorian year month $ fn nb
 
 fstMDay :: Day -> Day
-fstMDay day  =
-  let (year,month,_) = toGregorian day
-  in fromGregorian year month 01
+fstMDay day  = setDay 01 id day
 
 lstMDay :: Day -> Day
 lstMDay day =
   let (year,month,_) = toGregorian day
   in fromGregorian year month $ gregorianMonthLength year month
 
-  
-{- split fdate tdate  | sameM fdate tdate && sameY fdate tdate = [(fdate,tdate)]
-  where
-    sameM :: Day -> Day -> Bool
-    sameM fdate tdate = mm fdate == mm tdate
-    sameY :: Day -> Day -> Bool
-    sameY fdate tdate = yyyy fdate == yyyy tdate
-    mm :: Day -> Int
-    mm date = snd3 $ toGregorian date
-    yyyy date = fst3 $ toGregorian date
-split fdate tdate =(:) tuple  res
-  where
-    tuple = (,) fdate (eom fdate)
-    res = split (nDate' fdate) tdate
--}
---nDate' = undefined
---eom = undefined
 
-fn :: FromDate -> ToDate -> Either String [(FromDate,ToDate)]
-fn fdate tdate | not (isDateValid fdate && isDateValid tdate) = Left "err"
-fn fdate tdate |  sameMMYYYY =   return [ (,) fdate tdate]
-  where
-    sameMMYYYY  = sameMonth && sameYear
-    sameMonth   = dateMonth fdate == dateMonth tdate
-    sameYear    = dateYear fdate  == dateYear tdate
-fn fdate tdate  =  (:)  tuple <$>  res
-  where
-    tuple = (,) fdate (endOfMonth fdate)
-    res =  fn (nDate fdate) tdate
-    nDate d = case  (dateMonth d ) of
-      December -> Date ( (yyyy d) + 1) (month) 01
-      _        -> Date (yyyy d) (nMonth month) 01
-      where
-        month = mm d
-        years = yyyy d
 
-isDateValid :: Date -> Bool
-isDateValid date = undefined 
-
-    --endOfMonth = undefined
-endOfMonth :: Date -> Date
-endOfMonth date = Date (yyyy date) (mm date) maxDay
-  where
-    maxDay = daysInMonth (yyyy date) (mm date)
-
-nMonth :: Month -> Month
-nMonth mm = case mm of
-  January   -> February
-  February  -> March
-  March     -> April
-  April     -> May
-  May       -> June
-  June      -> July
-  July      -> August
-  August    -> September
-  September -> October
-  October   -> November
-  November  -> December
-  December  -> January
---nextMonth :: Date -> Date
---nextMonth date = Date (dateYear date) (dateMonth date) (nDays
---  where
- --   nDays = daysInMonth (dateYear date) (dateMonth date)
--- test/LeaveSpec.hs
+++ test/LeaveSpec.hs
@@ -1,12 +1,13 @@
 module LeaveSpec(spec) where
 
-import Test.Hspec
-import Test.QuickCheck
 
-import Data.Time 
-import Core.Leaves
+import           Data.Time
+import           Test.Hspec
+import           Test.QuickCheck
 
-import Core.Types
+
+import           Core.Leaves
+import           Core.Types
 
 spec :: Spec
 spec = do
@@ -15,9 +16,23 @@ spec = do
     let d1 = ModifiedJulianDay 15000
     let d2 = ModifiedJulianDay 18000
     split d1 d2 `shouldNotBe` []
-    
+
   describe "test split function for date in the same month" $
    it "should return a singelton list when the both date are in the same month" $ do
     let d1 = fromGregorian 2018 01 28
     let d2 = fromGregorian 2018 01 31
     split d1 d2 `shouldBe` [(d1,d2)]
+
+  describe "test fstMDay for January" $
+   it "should return (2018 01 01)" $ do
+    fstMDay (fromGregorian 2018 01 28) `shouldBe` fromGregorian 2018 01 01
+
+  describe "test lstMDay for February" $
+   it "should return the end of month " $ do
+    lstMDay (fromGregorian 2018 02 02) `shouldBe` fromGregorian 2018 02 28
+
+  describe "absCreation should create an absence journal based on a leave request" $
+   it "should fail if the leave request is not approved" $ property $
+    \leave -> case reqWorkflowState leave of
+      Pending  -> absCreation leave  == Nothing
+      Rejected -> absCreation leave  == Nothing
