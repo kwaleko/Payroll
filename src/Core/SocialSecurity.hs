78b63c4b48725aeb8321bbdb8e0af06b74e56b4d
kwaleko <khaledalomar@tutanota.com>
2018-07-28 09:56:54 +0300

    refactor

--- src/Core/SocialSecurity.hs
+++ src/Core/SocialSecurity.hs
@@ -6,7 +6,7 @@
 
 module Core.SocialSecurity where
 
-
+import                 Data.Time 
 import                 Core.Types
 import                 Control.Monad
 
@@ -16,25 +16,21 @@ class Monad m => GosiRules m where
   getRules :: m [Rule]
 
 class Monad m => GosiFormulas m where
-  getFormuls :: Paycode -> Nationality -> GosiType -> m  [Formula]
+  getFormuls :: Paycode -> Nationality -> GosiType -> m [Formula]
 
 class Monad m => EmployeeData m where
   getEmployee   :: EmpNumber -> m (Maybe Employee)
   getPycdAmnt   :: EmpNumber -> Paycode -> m (Maybe Amount)
 
 empShare :: (GosiRules m ,EmployeeData m,GosiFormulas m) => Employee -> PayrollConfig -> m [PayrollRecord]
-empShare Employee{..} pconf = do
-      let empNb = empNumber
+empShare emp PayrollConfig{..} = do
+      let empNb = empNumber emp
           age   = 10 -- empBirthDate emp
-          nat   = empNationality
-          ldate = empLastWorkingDate
-          pfrom = confFromDate pconf
-          pto   = confToDate pconf
       rules  <- getRules
-      pcodes <-  eligibleFor Employee{..} EmployeeShare pto
-      recs <- forM pcodes $ \pcode-> do
-        amount <- gosiCalc Employee{..} pcode
-        return $ PayrollRecord pfrom pto pcode empNb amount
+      pcodes <-  eligibleFor emp EmployeeShare confToDate
+      recs   <- forM pcodes $ \pcode-> do
+        amount <- gosiCalc emp pcode
+        return $ PayrollRecord confFromDate confToDate pcode empNb amount
       return $ filter ((/= 0) . recAmount) recs
 
 
@@ -42,11 +38,9 @@ empShare Employee{..} pconf = do
 -- Based on the formula specified in the setup of GOSI
 --eligibleFor :: (GosiRules m) => Age -> Nationality -> GosiType -> LastWorkingDate -> PeriodTo -> m [Paycode]
 eligibleFor :: (GosiRules m) => Employee -> GosiType -> PeriodTo -> m [Paycode]
-eligibleFor Employee{..} gType pTo  = do
+eligibleFor Employee{..} gType pTo = do
   let age     = 10
-      nat     = empNationality
-      lstDate = empLastWorkingDate
-  map rPaycode . filter (checkRules age nat lstDate) <$> getRules
+  map rPaycode . filter (checkRules age empNationality empLastWorkingDate) <$> getRules
   where
    checkRules :: Age -> Nationality -> LastWorkingDate -> Rule -> Bool
    checkRules  age nat lstDate rule
@@ -61,10 +55,8 @@ eligibleFor Employee{..} gType pTo  = do
 -- Why
 gosiCalc   :: (EmployeeData m,GosiFormulas m) => Employee -> Paycode -> m Amount
 gosiCalc Employee{..} pcode = do
-  let empNb = empNumber
-      nat   = empNationality
-  f   <- getFormuls pcode nat EmployeeShare
-  res <- traverse (applyF empNb) f
+  f   <- getFormuls pcode empNationality EmployeeShare
+  res <- traverse (applyF empNumber) f
   return $ foldl (+) 0 res
   where
     applyF ::(EmployeeData m) => EmpNumber -> Formula -> m Amount
