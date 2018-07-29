34a7a0c5f6a7c3f26ec668c318159e4ad2f3f67e
kwaleko <khaledalomar@tutanota.com>
2018-07-29 08:43:12 +0300

    add Leave deduction

--- src/Core/Leaves.hs
+++ src/Core/Leaves.hs
@@ -4,34 +4,56 @@ module Core.Leaves where
 
 import Core.Types
 
-import Data.Time(addGregorianMonthsClip
+import Control.Monad
+import Data.Maybe(isJust,fromJust)
+import Data.Time (addGregorianMonthsClip
                 ,gregorianMonthLength
-                ,Day(..)
                 ,toGregorian
-                ,fromGregorian)
+                ,fromGregorian
+                ,Day)
 
 
 class Monad m => HasLeave m where
   createLeave :: LeaveRequest -> m (Maybe LeaveId)
+
+class Monad m => HasLeaveSetup m where
   getLeaveSetup :: LeaveType -> m (Maybe LeaveSetup)
 
 class Monad m => HasAbsence m where
   createAbsence :: AbsenceJournal -> m ()
-  getAbsence :: PeriodFrom -> PeriodTo -> EmpNumber -> m [AbsenceJournal]
+  getAbsence    :: PeriodFrom -> PeriodTo -> EmpNumber -> m [AbsenceJournal]
 
 class Monad m => HasFormula m where
-  getFormulas :: m [Formula]
+  getFormulas :: LeaveType -> m [Formula]
 
+ -- return  [PayrollRecord  (fromGregorian 2019 01 01) (fromGregorian 2019 01 01) (Paycode " " "" Deduction GOSI ) "" 10]
+calcDeduction :: (HasLeave m,HasAbsence m,HasFormula m) => Employee -> PayrollConfig -> m [PayrollRecord]
+calcDeduction emp PayrollConfig{..} = do
+  let empNb = empNumber emp
+  abs <- filter (isJust . absPaycode ) <$> getAbsence confFromDate confToDate empNb
+  recs <- forM abs $ \absence -> do
+    let leavePcode = absPaycode absence
+    amount <- calcFormula absence
+    return $ PayrollRecord confFromDate confToDate (Leave (fromJust leavePcode )) empNb amount
+  return $ filter ((/=0) . recAmount ) recs
 
-calcDeduction :: (HasAbsence m) => Employee -> PayrollConfig -> m [PayrollRecord]
-calcDeduction emp pconf = return  [PayrollRecord  (fromGregorian 2019 01 01) (fromGregorian 2019 01 01) (Paycode " " "" Deduction GOSI ) "" 10]
+calcFormula ::(HasLeave m,HasFormula m) => AbsenceJournal -> m Amount
+calcFormula absence = undefined
 
-absGen :: LeaveRequest -> Maybe AbsenceJournal
-absGen LeaveRequest{..} =
+absGen :: (HasLeaveSetup m,HasAbsence m) => LeaveRequest -> m (Maybe AbsenceJournal)
+absGen LeaveRequest{..} = do
   let
     days     = split reqFromDate reqToDate
     desc     = "Create absence journal for " ++ show reqLeaveType
-  in if reqWorkflowState /= Approved then Nothing else Just $ AbsenceJournal  reqEmpl days desc
+  getLeaveSetup reqLeaveType >>= \case
+    Just val -> do
+      return $ if reqWorkflowState /= Approved
+        then Nothing
+        else do
+          --lift $ createAbsence $  AbsenceJournal  reqEmpl days (lsPaycode val) desc
+          Just $ AbsenceJournal  reqEmpl days (lsPaycode val) desc
+         
+    Nothing -> return Nothing
 
 -- the date for a leave request should be splited on
 -- a monthly basis so the payrun could deduct the
@@ -56,6 +78,3 @@ lstMDay :: Day -> Day
 lstMDay day =
   let (year,month,_) = toGregorian day
   in fromGregorian year month $ gregorianMonthLength year month
-
-
-
